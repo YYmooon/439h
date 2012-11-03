65836e8b1d6f8ebd4bf1f616e264df1176ab640e
Reid McKenzie <reid@cs.utexas.edu>
2012-11-03 14:27:41 -0500

    man fuck semicolons

--- kern/pmap.c
+++ kern/pmap.c
@@ -278,8 +278,8 @@ mem_init_mp(void)
     unsigned i;
     for(i = 0; i < NCPU; i++) {
         unsigned kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
-        boot_map_region(kern_pgdir, kstacktop_i - KSTKSIZE, KSTKSIZE, PADDR(&percpu_kstacks[i]), PTE_P | PTE_W)
-        boot_map_region(kern_pgdir, kstacktop_i - (KSTKSIZE + KSTKGAP), KSTKGAP, 0, 0)
+        boot_map_region(kern_pgdir, kstacktop_i - KSTKSIZE, KSTKSIZE, PADDR(&percpu_kstacks[i]), PTE_P | PTE_W);
+        boot_map_region(kern_pgdir, kstacktop_i - (KSTKSIZE + KSTKGAP), KSTKGAP, 0, 0);
     }    
 }
 
