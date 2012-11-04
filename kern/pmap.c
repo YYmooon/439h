e5b99ca7f481bcaefaf754f631f4fc2ab468ff81
Reid McKenzie <reid@cs.utexas.edu>
2012-11-04 01:25:10 -0600

    Exercise three

--- kern/pmap.c
+++ kern/pmap.c
@@ -275,7 +275,15 @@ mem_init_mp(void)
     //     Permissions: kernel RW, user NONE
     //
     // LAB 4: Your code here:
-
+    unsigned i;
+    for(i = 0; i < NCPU; i++) {
+        unsigned kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+        boot_map_region(kern_pgdir, 
+                        kstacktop_i - KSTKSIZE, 
+                        KSTKSIZE, 
+                        PADDR(&percpu_kstacks[i]), 
+                        PTE_P | PTE_W | PTE_P);
+    }
 }
 
 // --------------------------------------------------------------
