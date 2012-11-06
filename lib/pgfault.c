8c7ba10dc9096e2dec05255983ad6648bc464bd0
Reid McKenzie <reid@cs.utexas.edu>
2012-11-06 02:36:16 -0600

    untested set_pgfault_handler implementation

--- lib/pgfault.c
+++ lib/pgfault.c
@@ -29,7 +29,10 @@ set_pgfault_handler(void (*handler)(struct UTrapframe *utf))
     if (_pgfault_handler == 0) {
         // First time through!
         // LAB 4: Your code here.
-        panic("set_pgfault_handler not implemented");
+        if(!sys_page_alloc(0, UXSTACKTOP - PGSIZE + 1,  PTE_P | PTE_U | PTE_W))
+            panic("Unable to map a page for the user exception stack!\n");
+        if(!sys_env_set_pgfault_upcall(0,  &_pgfault_handler))
+            panic("Unable to set the user exception upcall!\n");
     }
 
     // Save handler pointer for assembly to call.
