5c82a8db86cbcb2db3624cbc53143f5450faa3f6
Reid McKenzie <reid@cs.utexas.edu>
2012-11-18 11:15:27 -0600

    Add a kernel-mode variant which uses curenv

--- debug.h
+++ debug.h
@@ -1,7 +1,18 @@
 #define DEBUG(...) \
 do {                                        \
-    cprintf("[%08x %-10s:%3d in %-16s] ",   \
+    cprintf("[%08x %-16s:%3d in %-24s] ",   \
             sys_getenvid(),                 \
+            __FILE__,                       \
+            __LINE__,                       \
+            __func__);                      \
+    cprintf(__VA_ARGS__);                   \
+    cprintf("\n");                          \
+} while(0);
+
+#define KDEBUG(...) \
+do {                                        \
+    cprintf("[%08x %-16s:%3d in %-24s] ",   \
+            curenv->env_id,                 \
             __FILE__,                       \
             __LINE__,                       \
             __func__);                      \
