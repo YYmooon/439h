123e791239910bed68f1183e63e496dff1b7ad40
Reid McKenzie <reid@cs.utexas.edu>
2012-12-01 17:06:34 -0600

    l6e5

--- kern/e1000.c
+++ kern/e1000.c
@@ -8,12 +8,22 @@
 int
 pci_e1000_attach(struct pci_func* f)
 {
-    e1000_reg_map =(char*) mmio_map_region(f->reg_base[0], 
+  // Map the registers using MMIO
+  e1000_reg_map =(char*) mmio_map_region(f->reg_base[0], 
                                            f->reg_size[0]);
-    DVR_DEBUG("trying to access MMIO'd memory...\n");
-    assert(*(uint32_t*)(e1000_reg_map + E1000_STATUS) == 0x80080783); 
-    DVR_DEBUG("sucess! device status is %08x\n", DEV_STATUS()); 
-    return 1;
+  DVR_DEBUG("trying to access MMIO'd memory...\n");
+  assert(*(uint32_t*)(e1000_reg_map + E1000_STATUS) == 0x80080783);  
+  DVR_DEBUG("sucess! device status is %08x\n", DEV_STATUS()); 
+
+  // Set the transmission h
+  *((unsigned*)(e1000_reg_map + E1000_TDLEN)) = 32*sizeof(struct tx_desc);
+  *((unsigned*)(e1000_reg_map + E1000_TDH)) = 0;
+  *((unsigned*)(e1000_reg_map + E1000_TDT)) = 0;
+
+  *((unsigned*)(e1000_reg_map + E1000_TCTL)) = (E1000_TCTL_EN | E1000_TCTL_PSP | 
+                                                E1000_TCTL_CT | E1000_TCTL_COLD);
+
+  return 1;
 }
 
 // LAB 6: Your driver code here
--- kern/e1000.h
+++ kern/e1000.h
@@ -5,10 +5,39 @@
 #include <kern/pmap.h>
 
 #define E1000_DEV_ID_82542  0x1000
-#define E1000_STATUS        0x00008  /* Device Status - RO */
+#define E1000_STATUS        0x00008       /* Device Status - RO */
+
+// transmission
+#define E1000_TCTL          0x00400       /* TX Control - RW */
+#define E1000_TDLEN         0x03808       /* TX Descriptor Length - RW */
+#define E1000_TDH           0x03810       /* TX Descriptor Head - RW */
+#define E1000_TDT           0x03818       /* TX Descripotr Tail - RW */
+#define E1000_TCTL_RST      0x00000001    /* software reset */
+#define E1000_TCTL_EN       0x00000002    /* enable tx */
+#define E1000_TCTL_BCE      0x00000004    /* busy check enable */
+#define E1000_TCTL_PSP      0x00000008    /* pad short packets */
+#define E1000_TCTL_CT       0x00000ff0    /* collision threshold */
+#define E1000_TCTL_COLD     0x003ff000    /* collision distance */
+#define E1000_TCTL_SWXOFF   0x00400000    /* SW Xoff transmission */
+#define E1000_TCTL_PBE      0x00800000    /* Packet Burst Enable */
+#define E1000_TCTL_RTLC     0x01000000    /* Re-transmit on late collision */
+#define E1000_TCTL_NRTU     0x02000000    /* No Re-transmit on underrun */
+#define E1000_TCTL_MULR     0x10000000    /* Multiple request support */
+
+struct tx_desc
+{
+  uint64_t addr;
+  uint16_t length;
+  uint8_t cso;
+  uint8_t cmd;
+  uint8_t status;
+  uint8_t css;
+  uint16_t special;
+};
 
 volatile char* e1000_reg_map;
 volatile char* e1000_flash_map;
+volatile struct tx_desc tx_descriptors[32];
 
 int pci_e1000_attach(struct pci_func* f);
 
