a6cb0a57ee0f5338d6fc179da8e9d0afb50123c1
Reid McKenzie <reid@cs.utexas.edu>
2012-12-01 23:24:25 -0600

    l6e6

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,29 +1,74 @@
-#include <inc/assert.h>
+#include <inc/error.h>
+
 #include <kern/e1000.h>
-#include <kern/env.h>
-#include <debug.h>
 
 #define DEV_STATUS() (*(uint32_t*)(e1000_reg_map + E1000_STATUS)) 
+
+volatile char* e1000_reg_map;
+volatile char* e1000_flash_map;
+struct tx_desc tx_descriptors[32];
+
+static int
+free_desc()
+{
+  int i;
+  volatile struct tx_desc* cursor;
+  for(i = 0; i < 32; i++) {
+    cursor =  (struct tx_desc*) tx_descriptors + i;
+    if(cursor->status & E1000_TXD_STAT_DD) {
+      return i;
+    }
+  }
+  return -E_UNSPECIFIED;
+}
+
+void
+zero_desc(volatile struct tx_desc* descriptor)
+{
+  descriptor->addr    = 0;
+  descriptor->length  = 0;
+  descriptor->cso     = 0;
+  descriptor->cmd     = 0;
+  descriptor->status  = 0;
+  descriptor->css     = 0;
+  descriptor->special = 0;
+}
 
 int
 pci_e1000_attach(struct pci_func* f)
 {
   // Map the registers using MMIO
-  e1000_reg_map =(char*) mmio_map_region(f->reg_base[0], 
-                                           f->reg_size[0]);
-  DVR_DEBUG("trying to access MMIO'd memory...\n");
-  assert(*(uint32_t*)(e1000_reg_map + E1000_STATUS) == 0x80080783);  
-  DVR_DEBUG("sucess! device status is %08x\n", DEV_STATUS()); 
+  e1000_reg_map = (char*) mmio_map_region(f->reg_base[0], 
+                                          f->reg_size[0]);
 
   // Set the transmission h
   *((unsigned*)(e1000_reg_map + E1000_TDLEN)) = 32*sizeof(struct tx_desc);
-  *((unsigned*)(e1000_reg_map + E1000_TDH)) = 0;
-  *((unsigned*)(e1000_reg_map + E1000_TDT)) = 0;
+  *((unsigned*)(e1000_reg_map + E1000_TDH))   = 0;
+  *((unsigned*)(e1000_reg_map + E1000_TDT))   = 0;
+  *((unsigned*)(e1000_reg_map + E1000_TCTL))  = (E1000_TCTL_EN | E1000_TCTL_PSP | 
+                                                 E1000_TCTL_CT | E1000_TCTL_COLD);
+  // Enable the DD bit..
+  *((unsigned*)(e1000_reg_map + E1000_TXDCTL)) |= E1000_TXD_CMD_RS;
 
-  *((unsigned*)(e1000_reg_map + E1000_TCTL)) = (E1000_TCTL_EN | E1000_TCTL_PSP | 
-                                                E1000_TCTL_CT | E1000_TCTL_COLD);
-
-  return 1;
+  return 0;
 }
 
-// LAB 6: Your driver code here
+int
+pci_e1000_tx(void* buffer, unsigned length)
+{
+  int r = free_desc();
+  if(r < 0) {
+    return -E_UNSPECIFIED;
+  } else {
+    volatile struct tx_desc* descriptor = (struct tx_desc*) &tx_descriptors[r];
+
+    zero_desc(descriptor);  
+
+    descriptor->addr = 0;
+    descriptor->length = length;
+    
+    *((unsigned*)(e1000_reg_map + E1000_TDT)) += 1;
+
+    return 0;
+  }
+}
