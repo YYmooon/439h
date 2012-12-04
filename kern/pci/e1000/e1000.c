f95b670265be90384a09f161d7ae926cb26dc156
Reid McKenzie <reid@cs.utexas.edu>
2012-12-04 12:46:40 -0600

    That should be a skeleton E1000 driver
      Okay. The driver is done, and much allong the lines of the MINIX e1000
    driver code to make it a little easier to debug. Now I need to throw the
    packet spamming code in the kernel just to have a testing packet source,
    and then I need to do the syscall API which should work nicely.

--- kern/pci/e1000/e1000.c
+++ kern/pci/e1000/e1000.c
@@ -1,19 +1,141 @@
+#include <inc/lib.h>
 #include <inc/assert.h>
-#include <kern/e1000.h>
+
 #include <kern/env.h>
+#include <kern/pci/e1000/e1000.h>
+#include <kern/pci/e1000/e1000_hw.h>
+
 #include <debug.h>
 
 #define DEV_STATUS() (*(uint32_t*)(e1000_reg_map + E1000_STATUS)) 
+#define incmod(x, b) (x = ((x + 1) % b))
 
+/*===========================================================================
+ * define the static variables which the following code will rely on
+ * were this a "real" driver, then these should be packeged in a struct
+ * which describes a single card, thus allowing the driver to serve multiple
+ * cards of the same type. See the MINIX E1000 driver for a perfect example.
+ *===========================================================================*/
+volatile char* e1000_reg_map;
+volatile char* e1000_flash_map;
+
+struct tx_buffer tx_buffers[E1000_RING_SIZE];
+struct tx_desc   tx_descriptors[E1000_RING_SIZE];
+
+/*=============================================================================
+ * helper code that I'll use to keep everything neat
+ *===========================================================================*/
+static void
+e1000_reg_assign(uint32_t reg, uint32_t val)
+{
+  *(uint32_t*)(e1000_reg_map + reg) = val;
+}
+
+static void
+e1000_reg_set(uint32_t reg, uint32_t val)
+{
+  *(uint32_t*)(e1000_reg_map + reg) |= val;
+}
+
+static void
+e1000_reg_unset_except(uint32_t reg, uint32_t val)
+{ 
+  *(uint32_t*)(e1000_reg_map + reg) &= val;
+}
+
+static void
+e1000_reg_unset(uint32_t reg, uint32_t val)
+{ 
+  *(uint32_t*)(e1000_reg_map + reg) &= ~val;
+}
+
+static void
+e1000_reg_clear(uint32_t reg)
+{ 
+  *(uint32_t*)(e1000_reg_map + reg) &= 0;
+}
+
+static uint32_t
+e1000_reg_read(uint32_t reg)
+{
+  return *(uint32_t*)(e1000_reg_map + reg);
+}
+
+/*===========================================================================
+ * The pci_attatch method,
+ * Takes a pci descriptor as generated in kern/pci.c and configures this device
+ * as a single-instance kernel mode driver to interact therewith
+ *===========================================================================*/
 int
 pci_e1000_attach(struct pci_func* f)
 {
-    e1000_reg_map =(char*) mmio_map_region(f->reg_base[0], 
-                                           f->reg_size[0]);
-    DVR_DEBUG("trying to access MMIO'd memory...\n");
-    assert(*(uint32_t*)(e1000_reg_map + E1000_STATUS) == 0x80080783); 
-    DVR_DEBUG("sucess! device status is %08x\n", DEV_STATUS()); 
-    return 1;
+  // reset just to be sure...
+  e1000_reg_set(E1000_CTRL, E1000_CTRL_RST);
+
+  // MMIO map the appropriate chunk to a local static pointer 
+  e1000_reg_map = (char*) mmio_map_region(f->reg_base[0], 
+                                          f->reg_size[0]);
+
+  DVR_DEBUG("trying to access MMIO'd memory...\n");
+  assert(*(uint32_t*)(e1000_reg_map + E1000_STATUS) == 0x80080783); 
+  DVR_DEBUG("sucess! device status is %08x\n", DEV_STATUS()); 
+
+  // Set up the transmit ring registers
+  // this code equivalent to the MINIX E1000 driver
+  e1000_reg_assign(E1000_TDBAL, PADDR(&tx_descriptors[0]));
+  e1000_reg_assign(E1000_TDBAH, 0);
+  e1000_reg_assign(E1000_TDLEN, E1000_RING_SIZE * sizeof(struct tx_desc));
+
+  e1000_reg_assign(E1000_TDH, 0);
+  e1000_reg_assign(E1000_TDT, 0);
+  e1000_reg_set(E1000_TCTL, E1000_TCTL_EN | E1000_TCTL_PSP);
+
+  // As the descriptor allocator relies on the dd bit being set,
+  // iterate through the descriptors and mark em all free
+  int i = 0;
+  for(i = 0; i < E1000_RING_SIZE; i++) {
+    tx_descriptors[i].status |= E1000_TXD_STAT_DD;
+  }
+
+  return 1;
 }
 
-// LAB 6: Your driver code here
+/*===========================================================================
+ * pci_tx configures a
+ * 
+ *===========================================================================*/
+int 
+pci_e1000_tx(void* buffer, uint32_t length)
+{
+  uint32_t head = e1000_reg_read(E1000_TDH),
+           tail = e1000_reg_read(E1000_TDT),
+           next = tail;
+
+  length = ((length < PGSIZE) ? length : PGSIZE);
+
+  struct tx_desc* d = &tx_descriptors[next];
+  for(; 
+      (d->status | E1000_TXD_STAT_DD); 
+      incmod(next, E1000_RING_SIZE)) {
+    d = &tx_descriptors[next];
+  }
+
+  assert(d->status | E1000_TXD_STAT_DD);
+
+  // copy the data to an internal buffer, truncating to pagesize
+  memcpy(&tx_buffers[next], buffer, length);
+
+  // unset the DD bit
+  d->status &= ~E1000_TXD_STAT_DD;
+  // set the address
+  d->addr = PADDR(&tx_buffers[next]);
+  // set the length
+  d->length = length;
+  // set the command bits
+  d->cmd |=  E1000_TXD_CMD_EOP | E1000_TXD_CMD_RS;
+
+  // increment the tail value to whatever next is...
+  e1000_reg_assign(E1000_TDT, next);
+
+  return length;
+}
--- kern/pci/e1000/e1000.h
+++ kern/pci/e1000/e1000.h
@@ -3,13 +3,28 @@
 #include <inc/types.h>
 #include <kern/pci.h>
 #include <kern/pmap.h>
-#include <kern/pci/e1000/e1000_hw.h>
 
-#define E1000_DEV_ID_82542  0x1000
-#define E1000_STATUS        0x00008  /* Device Status - RO */
+#define E1000_RING_SIZE 32
 
-volatile char* e1000_reg_map;
-volatile char* e1000_flash_map;
+struct tx_buffer {
+  char data[PGSIZE];
+}__attribute__((align(PGSIZE)));
+
+struct tx_desc
+{
+  uint64_t addr;
+  uint16_t length;
+  uint8_t cso;
+  uint8_t cmd;
+  uint8_t status;
+  uint8_t css;
+  uint16_t special;
+}__attribute__((align(16)));
+
+extern volatile char* e1000_reg_map;
+extern volatile char* e1000_flash_map;
+extern struct tx_buffer tx_buffers[E1000_RING_SIZE];
+extern struct tx_desc   tx_descriptors[E1000_RING_SIZE];
 
 int pci_e1000_attach(struct pci_func* f);
 int pci_e1000_tx(void* buffer, uint32_t length);
