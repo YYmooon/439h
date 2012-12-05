604ed1763eca91648df0e58c73b6dff8325a418f
Reid McKenzie <reid@cs.utexas.edu>
2012-12-05 01:45:48 -0600

    forgot the prototype

--- inc/lib.h
+++ inc/lib.h
@@ -65,7 +65,7 @@ int sys_env_disable_preempt();
 int sys_env_enable_preempt();
 int sys_env_recovered();
 int sys_net_send(void*, uint32_t);
-int sys_net_blocking_send(void*, uint32_t);
+int sys_net_recieve(void*, uint32_t);
 
 // This must be inlined.  Exercise for reader: why?
 static __inline envid_t __attribute__((always_inline))
